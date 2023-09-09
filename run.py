a5788fd458935694492bd9dc9d72345afa5ff412
CherrySaz <>
2023-09-09 17:06:18 +0000

    set up main functions for connect 4 game

---
+++ run.py
@@ -0,0 +1,29 @@
+import random
+
+# functions
+"""
+  This is the creation of the game, connect 4 board.
+  The board has rows and columns.
+  Returns the game board in a list of lists.
+"""
+
+
+def create_board(rows, cols):
+    return [[' ' for _ in range(cols)] for _ in range(rows)]
+
+
+def print_board(board):
+
+
+def is_vaild_move():
+
+
+def make_move(board, col, player):
+
+
+def computer_move(board):
+    
+
+def main():
+
+main()  
